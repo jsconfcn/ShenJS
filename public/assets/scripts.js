75f71ea8874c85afc65c1862efb665bb7e09d0c0
z4rd <z4rd@live.cn>
2015-03-04 13:30:06 +0800

    add call for speakers & style adjustments

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -9788,35 +9788,33 @@ $(function() {
   });
 });
 $(function() {
-  function center(){
-    var header = $('#header');
-    if (header.length) {
-      // Reset for calculations
-      header.css({ 'height': '' });
-      // Apply our magic
-      var viewport = $(window).height();
-      var position = header.offset();
-      header.css({
-        'height': viewport
-      });
-    }
-  }
-  function firstlaunch(){
-      $('#header').css({'position':'relative'});
-      $('#content').css({'padding':'0'});
-  }
+  // function center(){
+  //   var header = $('#header');
+  //   if (header.length) {
+  //     // Reset for calculations
+  //     header.css({ 'height': '' });
+  //     // Apply our magic
+  //     var viewport = $(window).height();
+  //     var position = header.offset();
+  //     header.css({
+  //       'height': viewport
+  //     });
+  //   }
+  // }
+  // function firstlaunch(){
+  //     $('#header').css({'position':'relative'});
+  //     $('#content').css({'padding':'0'});
+  // }
   $('.arrow').click(function(){
     if($(this).hasClass('register')){
       $('html,body').stop().animate({scrollTop: $('#register').offset().top },300);
-      console.log($('#register').offset().top);
-
     }else if($(this).hasClass('down')){
-      $('html,body').stop().animate({scrollTop: $(window).height()},300);
+      $('html,body').stop().animate({scrollTop: $('.action').offset().top },300);
     }
   });
-  center();
-  firstlaunch();
-  $(window).resize(center);
+  // center();
+  // firstlaunch();
+  // $(window).resize(center);
 });
 hljs.initHighlightingOnLoad();
 $(function() {
