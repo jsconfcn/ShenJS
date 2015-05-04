4bf734b67b8dfb46eb5fa5fd99a98a2f5edcde01
wwayne <wayneup@163.com>
2015-05-04 18:39:00 +0800

    add header and css and js helper

--- js/index.js
+++ js/index.js
@@ -6,10 +6,23 @@ window.onload = function() {
 window.onresize = function(){
   setSection();
 }
-// Set every part stretch the browser
+window.onscroll = function() {
+  var top = document.getElementById("header").getBoundingClientRect().bottom;
+  var menu = document.getElementById("menu");
+  var menuHeight = menu.clientHeight;
+  var menuClass = menu.className;
+  if(top-menuHeight < 0 && menuClass.indexOf("fixed") === -1) {
+    menu.className = menuClass + " fixed";
+  }
+  else if(top-menuHeight > 0 && menuClass.indexOf("fixed") !== -1) {
+    menu.className = "menu";
+  }
+}
+
+// Set every part stretch to the as same height as the browser
 function setSection() {
   var section = document.querySelectorAll('.stretch-full');
-  var windowHeight = document.body.clientHeight+"px";
+  var windowHeight = document.body.clientHeight + "px";
   for(var i = 0; i < section.length; i++) {
     section[i].style.height = windowHeight;
   }
